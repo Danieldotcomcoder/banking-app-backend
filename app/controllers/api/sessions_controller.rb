755ddb72336a9c9cd0d8aae96a2d36a33f06f8fc
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-06-22 11:39:35 +0300

    Add the session create method

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,15 +1,17 @@
-module API 
-class SessionsController < ApplicationController
+# frozen_string_literal: true
 
- def create 
+module API
+  class SessionsController < ApplicationController
+    def create
+      user = User.where(email: params[:email]).first
+      if user&.valid_password?(params[:password])
 
- end
+        render json: user.as_json(only: %i[id email]), status: :created
+      else
+        head(:unauthorized)
+      end
+    end
 
-
- def destroy
-
- end
-
-end
-
+    def destroy; end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
-  
+
   namespace :api do
-    resources :sessions, only: [:create, :destroy] 
+    resources :sessions, only: %i[create destroy]
     resources :accounts
-
-
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
