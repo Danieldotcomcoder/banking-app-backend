67ec4b5bd334df65d16e913f13b9ad309855bc32
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-06-26 12:10:36 +0300

    Re-add Validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
-  # validates :transaction_type, presence: true
-  # validates :total_amount, presence: true
+  belongs_to :account
+  has_one :payment
+
+  validates :transaction_type, presence: true
+  validates :total_amount, presence: true
 end
