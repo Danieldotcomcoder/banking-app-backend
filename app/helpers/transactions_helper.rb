44c4d01e305c7125f0b2ef127ae9ed295f03b1b0
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-07-06 16:53:36 +0300

    Fix credit card update

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,28 +1,32 @@
 # frozen_string_literal: true
 
 module TransactionsHelper
-  # def transaction_type_controller(payment)
-  #   case payment.payment_type
+  def transaction_type_controller(payment)
+    case payment.payment_type
 
-  #   when 'Cash'
-  #     Cash.create(cash_amount: payment.amount, payment_id: payment.id)
-  #     true
-  #   when 'CreditCard'
-  #     if check_credit_card(payment.account_id) == true
-  #       @creditcard = CreditCard.find_by(account_id: payment.account_id)
-  #       newbalance = @creditcard.card_balance - payment.amount
-  #       @creditcard.update(card_balance: newbalance)
-  #       true
-  #     else
-  #       false
-  #     end
+    when 'Cash'
+      Cash.create(cash_amount: payment.amount, payment_id: payment.id)
+      true
+    when 'CreditCard'
+      if check_credit_card(payment.account_id) == true
+        @creditcard = CreditCard.find_by(account_id: payment.account_id)
+        newbalance = @creditcard.card_balance - payment.amount
+        @creditcard.update(card_balance: newbalance)
+        true
+      else
+        false
+      end
 
-  #   end
-  # end
+    end
+  end
 
-  def self.check_credit_card
-    @cc = CreditCard.find_by(id: 1)
-    puts @cc
+  def check_credit_card(accid)
+    if CreditCard.find_by(account_id: accid).nil?
+      false
+    else
+      true
+  
   end
 end
-TransactionsHelper.check_credit_card
+end
+
